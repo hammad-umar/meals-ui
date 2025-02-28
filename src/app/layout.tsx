9f6c1983e48e3f0f0b0ee24de7a3ffa88f221742
Hammad Umar <hammadhawc227@gmail.com>
2025-02-28 23:06:06 +0500

    chore: cleanup the project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,13 @@
 import { Metadata } from 'next';
-
-import './globals.css';
-
 import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({
+  subsets: ['latin'],
+});
 
 export const metadata: Metadata = {
-  title: `TypeScript starter for Next.js by João Pedro Schmitz`,
-  description: `TypeScript starter for Next.js that includes all you need to build amazing apps`,
+  title: `Meals Planner`,
+  description: `Plan meals for your loved ones.`,
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
