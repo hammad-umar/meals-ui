8b251030fc3542303e328375636f1c4d251bc132
Hammad Umar <hammadhawc227@gmail.com>
2025-03-01 23:36:39 +0500

    chore: setup tailwindcss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { NextPage } from 'next';
 const HomePage: NextPage = () => {
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1 className="text-red-900 text-4xl">Home Page</h1>
     </div>
   );
 };
