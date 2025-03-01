943c6d4b972b83d7a6f146e1b8b4e4e07de7b239
Hammad Umar <hammadhawc227@gmail.com>
2025-03-01 23:39:45 +0500

    chore: setup shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import { NextPage } from 'next';
 
+import { Button } from '@/components/ui/button';
+
 const HomePage: NextPage = () => {
   return (
     <div>
       <h1 className="text-red-900 text-4xl">Home Page</h1>
+      <Button>Hello</Button>
     </div>
   );
 };
